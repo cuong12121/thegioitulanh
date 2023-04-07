e1c9aaba17c339b25b7acb38cd5aa3f4b1603f05
Cuongdao2222 <cuongdao12121@gmail.com>
2023-04-07 11:47:59 +0700

    Update apps.blade.php

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -122,7 +122,7 @@
          @stack('css')
     </head>
     <body class="module-home view-home">
-        <script src="{{  asset('http://code.jquery.com/jquery-1.10.2.js') }}"></script>
+        <script src="{{  asset('js/jquery-1.10.2.js') }}"></script>
 
         <!-- <script src="http://code.jquery.com/ui/1.11.4/jquery-ui.js"></script> -->
         <div class="wrapper">
