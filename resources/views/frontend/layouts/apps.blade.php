4460057181f122164511789fff7a2204b4bd0e62
Cuongdao2222 <cuongdao12121@gmail.com>
2023-05-02 10:41:48 +0700

    Update apps.blade.php

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -40,6 +40,15 @@
         <meta property="og:type" content="website" />
        
         <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5K92Z5Z');</script>
+        <!-- End Google Tag Manager -->
         <!-- Array
             (
                 [name] => home
@@ -401,6 +410,14 @@
                 })
             });    
         </script>
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5K92Z5Z');</script>
+        <!-- End Google Tag Manager -->
        
        
     </body>
